51eb5bb37bbac79942e8865db8338b490a6cab44
BISCVITE30 <gonnki2004@gmail.com>
2024-03-30 16:17:30 +0100

    first commit, make some work

--- scripts/calendar/header.js
+++ scripts/calendar/header.js
@@ -1,4 +1,4 @@
-import { getItem } from '../common/storage.js';
+import { getItem, setItem } from '../common/storage.js';
 import { generateWeekRange } from '../common/time.utils.js';
 import { openModal } from '../common/modal.js';
 
@@ -9,7 +9,34 @@ export const renderHeader = () => {
   // на основе полученного массива сформируйте разметку в виде строки - 7 дней (день недели и число в месяце)
   // полученную разметку вставить на страницу с помощью innerHTML в .calendar__header
   // в дата атрибуте каждой ячейки должно хранить для какого часа эта ячейка
-};
+  const getHeader = document.querySelector('.calendar__header');
+  const today = new Date();
+  const displayedWeekStart = new Date();
 
+  const firstDayOfWeek = new Date(today);
+  firstDayOfWeek.setDate(today.getDate() - today.getDay());
+
+  daysOfWeek.forEach((day, index) => {
+    const dateContainer = document.createElement('div');
+    dateContainer.classList.add('calendar__day-label');
+    const dayOfWeek = document.createElement('div');
+    dayOfWeek.classList.add('day-label__day-name');
+    dayOfWeek.textContent = day;
+    const numOfDay = document.createElement('div');
+    numOfDay.classList.add('day-label__day-num');
+
+    const currentDay = new Date(firstDayOfWeek);
+    currentDay.setDate(firstDayOfWeek.getDate() + index);
+    const dayOfMonth = currentDay.getDate();
+    numOfDay.textContent = dayOfMonth;
+
+    dateContainer.setAttribute('date-data', dayOfMonth);
+
+    dateContainer.append(dayOfWeek, numOfDay);
+    getHeader.append( dateContainer);
+  });
+  getItem(displayedWeekStart);
+  setItem(displayedWeekStart, firstDayOfWeek);
+};
 // при клике на кнопку "Create" открыть модальное окно с формой для создания события
 // назначьте здесь обработчик
--- scripts/common/createNumbersArray.js
+++ scripts/common/createNumbersArray.js
@@ -1,3 +1,8 @@
 export const createNumbersArray = (from, to) => {
   // ф-ция должна генерировать массив чисел от from до to
+  const arrOfNum = [];
+  for(let i = from; i <= to; i += 1){
+    arrOfNum.push(i);
+  }
+  return arrOfNum;
 };
