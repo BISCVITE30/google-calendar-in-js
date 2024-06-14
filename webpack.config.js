e5a5cf8bd026c600a0806729f06694a4ba783106
BISCVITE30 <gonnki2004@gmail.com>
2024-06-14 18:20:54 +0200

    !

--- webpack.config.js
+++ webpack.config.js
@@ -4,45 +4,45 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = (_, argv) => {
-    const isProduction = argv.mode === 'production';
-    const config = {
-        entry: './src/scripts/index.js',
-        output: {
-            filename: './bundle.js',
+  const isProduction = argv.mode === 'production';
+  const config = {
+    entry: './src/scripts/index.js',
+    output: {
+      filename: './bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.s?css$/i,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+            'css-loader',
+            'sass-loader',
+          ],
         },
-        module: {
-            rules: [
-                {
-                    test: /\.s?css$/i,
-                    use: [
-                        isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
-                        'css-loader',
-                        'sass-loader',
-                    ],
-                },
-            ]
-        },
-        plugins: [
-            new webpack.ProgressPlugin(),
-            new CleanWebpackPlugin(),
-            new HtmlWebpackPlugin({
-                template: './src/index.html',
-            }),
-        ],
+      ],
+    },
+    plugins: [
+      new webpack.ProgressPlugin(),
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+      }),
+    ],
 
-        devServer: {
-        port: 9000,
-        hot: true,
-        },
-    };
+    devServer: {
+      port: 9000,
+      hot: true,
+    },
+  };
 
-    if(isProduction) {
-        config.plugins.push(
-            new MiniCssExtractPlugin({
-                filename: '[name].css'
-            })
-        );
-    }
+  if (isProduction) {
+    config.plugins.push(
+      new MiniCssExtractPlugin({
+        filename: '[name].css',
+      }),
+    );
+  }
 
-    return config;
-}
+  return config;
+};
